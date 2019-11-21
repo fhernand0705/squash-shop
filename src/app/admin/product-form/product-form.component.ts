175a819699706ea7f46452dddd8791b044cae562
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-11-21 12:40:08 -0500

    added built-in and custom validators

---
+++ src/app/admin/product-form/product-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-form',
+  templateUrl: './product-form.component.html',
+  styleUrls: ['./product-form.component.scss']
+})
+export class ProductFormComponent {
+  categories$;
+
+  constructor (
+    categoryService: CategoryService,
+    private productService: ProductService
+  ) { this.categories$ = categoryService.getCategories(); }
+
+  save(product) {
+    this.productService.create(product);
+  }
+}
